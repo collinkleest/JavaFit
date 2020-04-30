75c28b08864c1cd7a41e86aec2ec733b02eddcf7
Collin Kleest <collinkleest@gmail.com>
2020-04-30 12:15:26 -0400

    ui changes -dynamic routine page - mongo queries

--- src/main/java/com/javafit/View/RoutineView.java
+++ src/main/java/com/javafit/View/RoutineView.java
@@ -7,19 +7,24 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import org.bson.Document;
 import org.kordamp.bootstrapfx.scene.layout.Panel;
 
 import com.javafit.Controller.CustomRoutineController;
 import com.javafit.Controller.DashController;
 import com.javafit.View.RegistrationView;
 import com.jfoenix.controls.JFXButton;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 
@@ -29,7 +34,7 @@ public class RoutineView {
 	private String userName;
 	private Stage routineStage;
 	private MongoClient mongoClient;
-	private MongoDatabase usersDB;
+	private MongoDatabase routinesDB;
 
 	
 	public RoutineView(String userName) throws IOException{
@@ -38,8 +43,20 @@ public class RoutineView {
         this.routineScene = new Scene(loadFXML(), 956, 800);
         this.routineStage.setScene(this.routineScene);
 		
+		ScrollPane mainPane = (ScrollPane) this.routineScene.lookup("#mainPane");
+        this.initializeMongoConnection();
+        MongoCollection<Document> routinesCollection = this.routinesDB.getCollection("ROUTINES");
+		FindIterable<Document> iterable= routinesCollection.find();
+		MongoCursor<Document> cursor = iterable.iterator();
 		
-        
+		try {
+			while(cursor.hasNext()) {
+				System.out.println(cursor.next());
+			}
+		} finally {
+			cursor.close();
+		}
+		
         JFXButton dashBtn =  (JFXButton) this.routineScene.lookup("#dashBtn");
         dashBtn.setOnAction((ActionEvent event) -> {
         	Stage stage = (Stage) dashBtn.getScene().getWindow();
@@ -71,7 +88,7 @@ public class RoutineView {
 	private void initializeMongoConnection() {
         this.mongoClient = MongoClients.create(
                 "mongodb+srv://ckleest:ckk@javafit-qy8fa.mongodb.net/test?retryWrites=true&w=majority");
-        this.usersDB = mongoClient.getDatabase("ROUTINES");
+        this.routinesDB = mongoClient.getDatabase("ROUTINES");
     }
 
     public void closeMongoConnection() {
